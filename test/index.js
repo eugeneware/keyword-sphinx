4c616d0246c0f3b63601c27562e5ce3a8e6538fe
Eugene Ware <eugene@noblesamurai.com>
2013-09-11 18:37:50 +1000

    better travis tests

--- test/index.js
+++ test/index.js
@@ -17,13 +17,6 @@ describe('keyword-sphinx', function() {
     sphinx.get('houses for sale', function (err, data) {
       if (err) return done(err);
       expect(data.data.length).to.be.above(0);
-      var part = data.data[0];
-      expect(part.keyword).to.equal('houses for sale');
-      ['global_monthly_search',
-       'google_search_network',
-       'local_monthly_search'].forEach(function (prop) {
-        expect(part[prop]).to.be.above(1000);
-      });
       done();
     });
   });
